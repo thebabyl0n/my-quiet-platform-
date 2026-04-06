79e75839c8e086bd89a4f8f87b2b62df0be84f04
thebabyl0n <newmyacc113@gmail.com>
2026-04-06 16:32:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.secret_key = os.environ.get('SECRET_KEY', 'hamada_super_secret_77')
 
 # إعداد الذكاء الاصطناعي
-API_KEY = "AIzaSyBFDDRMctvn8btilW6VXgQxJkvod_6hUZw"
+API_KEY = "AIzaSyB7vEt8yjAiBL_Ou5CewF8DBmwnypXDtyM"
 genai.configure(api_key=API_KEY)
 
 # إعداد الموديل مع "تعليمات النظام" عشان الشخصية تثبت
